dc1c7c87a788f35c1f41cf9164882279da5ce2a4
nzemia <mualukofrank@gmail.com>
2024-07-04 13:02:00 +0300

    fetching store products

--- src/app/secret-dashboard/actions.ts
+++ src/app/secret-dashboard/actions.ts
@@ -36,17 +36,63 @@ export async function CreatePostAction({
     return { succes: true, post: newPost }
 }
 
-export async function GetAllProductsAction() {
+//function to check if the user is admin, and calling it instead  of creating in each action
+async function CheckIfUserIsAdmin() {
     const { getUser } = getKindeServerSession()
     const user = await getUser()
 
     const isAdmin = user?.email === process.env.ADMIN_EMAIL
 
-    if (!user || !isAdmin) {
-        throw new Error("Only admin users can create posts!")
+    if (!user || !isAdmin) return false
+
+    return true
+}
+
+export async function GetAllProductsAction() {
+    const isAdmin = await CheckIfUserIsAdmin()
+
+    if (!isAdmin) {
+        throw new Error("Only admin users can add new products!")
     }
 
     const products = await prisma.product.findMany()
 
     return products
+}
+
+type ProductArgs = {
+    name: string
+    image: string
+    price: string
+}
+export async function AddNewProductToStoreAction({
+    name,
+    image,
+    price
+}: ProductArgs) {
+    const isAdmin = await CheckIfUserIsAdmin()
+
+    if (!isAdmin) {
+        throw new Error("Only admin users can add new products!")
+    }
+
+    if (!name || !image || !price) {
+        throw new Error("All fields are required!")
+    }
+
+    const priceInCents = Math.round(parseFloat(price) * 100)
+
+    if (isNaN(priceInCents)) {
+        throw new Error("Price must be a valid number!")
+    }
+
+    const newProduct = await prisma.product.create({
+        data: {
+            name,
+            image,
+            price: priceInCents
+        }
+    })
+
+    return { success: true, product: newProduct }
 }
