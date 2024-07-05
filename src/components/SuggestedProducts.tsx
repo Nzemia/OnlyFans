94341dadcbf24699a811e66f66907040508e43f7
nzemia <mualukofrank@gmail.com>
2024-07-05 10:48:28 +0300

    fetching the recommended products

--- src/components/SuggestedProducts.tsx
+++ src/components/SuggestedProducts.tsx
@@ -1,8 +1,15 @@
-import { products } from "@/dummy_data"
+import prisma from "@/db/prisma"
 import SuggestedProduct from "./SuggestedProduct"
 
+const SuggestedProducts = async () => {
+    const products = await prisma.product.findMany({
+        where: {
+            isArchived: false
+        },
 
-const SuggestedProducts = () => {
+        take: 4
+    })
+
     return (
         <div className="lg:w-2/5 hidden lg:flex  flex-col gap-3 px-2 sticky top-0 right-0 h-screen ml-3">
             <div className="flex flex-col gap-2 mt-20">
@@ -11,12 +18,11 @@ const SuggestedProducts = () => {
                 </p>
 
                 <div className="grid grid-cols-2 gap-4">
-                    {products.map((product) => (
+                    {products.map(product => (
                         <SuggestedProduct key={product.id} product={product} />
                     ))}
                 </div>
             </div>
-            
         </div>
     )
 }
