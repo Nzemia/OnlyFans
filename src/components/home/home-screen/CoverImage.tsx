41594dc5f84e9482c6df2a03ae9f8fa7f031282b
nzemia <mualukofrank@gmail.com>
2024-07-05 15:17:07 +0300

    updated cover page to display likes, posts and videos

--- src/components/home/home-screen/CoverImage.tsx
+++ src/components/home/home-screen/CoverImage.tsx
@@ -1,56 +1,78 @@
-import { Heart, ImageIcon, VideoIcon } from 'lucide-react'
-import Image from 'next/image'
-import React from 'react'
+import prisma from "@/db/prisma"
+import { Heart, Image as ImageIcon, VideoIcon } from "lucide-react"
+import Image from "next/image"
+import React from "react"
 
-const CoverImage = () => {
+const CoverImage = async () => {
+    const imageCount = await prisma.post.count({
+        where: {
+            mediaType: "image"
+        }
+    })
+    const videoCount = await prisma.post.count({
+        where: {
+            mediaType: "video"
+        }
+    })
+
+    const totalLikes = await prisma.post.aggregate({
+        _sum: {
+            likes: true
+        }
+    })
+
+    //format the number of likes , 10K, 1M,...
+    function formatNumber(num: number) {
+        if (num >= 1000000) {
+            return (num / 1000000).toFixed(1).replace(/\.0$/, "") + "M"
+        }
+        if (num >= 1000) {
+            return (num / 1000).toFixed(1).replace(/\.0$/, "") + "K"
+        }
+        return num.toString()
+    }
+
     return (
-        <div className='h-44 overflow-hidden relative'>
-            <Image 
+        <div className="h-44 overflow-hidden relative">
+            <Image
                 src={"/featured/1.jpg"}
-                alt='featured cat cover image'
+                alt="featured cat cover image"
                 fill
-                className='h-full w-full object-cover select-none pointer-events-none'
+                className="h-full w-full object-cover select-none pointer-events-none"
             />
-            <div className='absolute top-0 left-0 w-full h-full bg-gradient-to-b from-slate-800 to-transparent'
+            <div
+                className="absolute top-0 left-0 w-full h-full bg-gradient-to-b from-slate-800 to-transparent"
                 aria-hidden="true"
             />
 
             <div className="flex justify-between items-center absolute top-0 left-0 px-2 py-1 z-20 w-full">
                 <div className="flex items-center gap-2">
                     <div className="flex flex-col text-white ">
-                        <p className='font-bold'>
-                            Frank
-                        </p>
+                        <p className="font-bold">Frank</p>
 
                         <div className="flex gap-2 items-center">
                             <div className="flex items-center gap-1">
-                                <ImageIcon                                    
-                                    className='w-4 h-4'
-                                />
-                                <span className='text-sm font-bold'>
-                                    45
-                                </span>
-                            </div>
-                            
-                            <span className='text-xs'>•</span>
-
-                            <div className="flex items-center gap-1">
-                                <VideoIcon                                    
-                                    className='w-4 h-4'
-                                />
-                                <span className='text-sm font-bold'>
-                                    67
+                                <ImageIcon className="w-4 h-4" />
+                                <span className="text-sm font-bold">
+                                    {imageCount}
                                 </span>
                             </div>
 
-                            <span className='text-xs'>•</span>
+                            <span className="text-xs">•</span>
 
                             <div className="flex items-center gap-1">
-                                <Heart                                   
-                                    className='w-4 h-4'
-                                />
-                                <span className='text-sm font-bold'>
-                                    10K
+                                <VideoIcon className="w-4 h-4" />
+                                <span className="text-sm font-bold">
+                                    {videoCount}
+                                </span>
+                            </div>
+
+                            <span className="text-xs">•</span>
+
+                            <div className="flex items-center gap-1">
+                                <Heart className="w-4 h-4" />
+                                <span className="text-sm font-bold">
+                                    {formatNumber(totalLikes._sum.likes || 0)}
                                 </span>
                             </div>
                         </div>
