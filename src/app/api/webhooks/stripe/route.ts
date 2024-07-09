c821495fe2e31aafbf56f194f46047899a8c2945
nzemia <mualukofrank@gmail.com>
2024-07-09 23:48:03 +0300

    added checkoutsession expired

--- src/app/api/webhooks/stripe/route.ts
+++ src/app/api/webhooks/stripe/route.ts
@@ -217,6 +217,15 @@ export async function POST(req: Request) {
                 }
                 break
             }
+            case "checkout.session.expired": {
+                const session = await stripe.checkout.sessions.retrieve(
+                    (data.object as Stripe.Checkout.Session).id
+                )
+                await prisma.order.delete({
+                    where: { id: session.metadata!.orderId }
+                })
+                break
+            }
             default:
                 console.warn(`Unhandled event type: ${eventType}`)
                 break
